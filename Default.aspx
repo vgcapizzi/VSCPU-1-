44966eac59df1c5dd17392b72bbba810f6e8556f
vgcapizzi <vgcapizzi@gmail.com>
2017-01-16 19:27:41 -0500

    Entire VSCPU Folder

---
+++ Default.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <asp:Button ID="Button1" runat="server" Height="63px" OnClick="Button1_Click" Text="Button" Width="344px" />
+    <asp:TextBox ID="TextBox1" runat="server" Height="95px" OnTextChanged="TextBox1_TextChanged" Width="272px"></asp:TextBox>
+    <asp:TextBox ID="TextBox2" runat="server" OnTextChanged="TextBox2_TextChanged" Width="273px"></asp:TextBox>
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+
+
+
+public partial class _Default : Page
+{
+   
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+
+        string str = TextBox1.Text;
+        char[] str1 = str.ToCharArray();
+        byteObject input = new byteObject();
+        //str = input.altValue(str);
+        string str2 = input.toHex(str1);
+        
+        TextBox2.Text = str2;
+
+
+        return;
+    }
+    class memCell
+    {
+        int[,] cell = new int[67, 67]; //mem cell 2d array 64 by 64
+        for (int row = 0; row < 67; row++) //fill array by default with all 0
+        {
+            
+            for(int col = 0; col > 67; col++)
+            {
+                cell[row,col] = 
+            }
+        }
+
+    }
+
+    class byteObject 
+    {
+        int dec;
+        char[] bin = { '0', '0', '0', '0', ' ', '0', '0', '0', '0' };
+        char[] hex = { '0', ' ', '0'};
+        string machineCode;
+        
+        
+        public string altValue(string assemble) //method assemble to bin with proper instrc bits
+        {
+            string input = assemble;
+            switch(input)
+            {
+                case "ADD":
+                    bin[0] = '0';
+                    bin[1] = '0';
+                    break;
+                case "AND":
+                    bin[0] = '0';
+                    bin[1] = '1';
+                    break;
+                case "JMP":
+                    bin[0] = '1';
+                    bin[1] = '0';
+                    break;
+                case "INC":
+                    bin[0] = '1';
+                    bin[1] = '1';
+                    break;
+            }
+            machineCode = new string(bin);
+            return machineCode;
+        }
+        public string toHex(char[] bin) //method bin to hex
+        {
+            string input = new string(bin);
+            string input1 = input.Substring(0,4);
+            string input2 = input.Substring(5,4); 
+            int hexValue1 = Convert.ToInt32(input1, 2);
+            int hexValue2 = Convert.ToInt32(input2, 2);
+            string output1 = hexValue1.ToString();
+            string output2 = hexValue2.ToString();
+
+            switch(output1)
+            {
+                case "10":
+                    output1 = "A";
+                    break;
+                case "11":
+                    output1 = "B";
+                    break;
+                case "12":
+                    output1 = "C";
+                    break;
+                case "13":
+                    output1 = "D";
+                    break;
+                case "14":
+                    output1 = "E";
+                    break;
+                case "15":
+                    output1 = "F";
+                    break;
+            }
+            switch (output2)
+            {
+                case "10":
+                    output2 = "A";
+                    break;
+                case "11":
+                    output2 = "B";
+                    break;
+                case "12":
+                    output2 = "C";
+                    break;
+                case "13":
+                    output2 = "D";
+                    break;
+                case "14":
+                    output2 = "E";
+                    break;
+                case "15":
+                    output2 = "F";
+                    break;
+            }
+            //could have done this better, prob dont need two case statements for this
+            string output = output1 + " " + output2;
+            return output;
+        }
+        
+        //try catches for wrong input data
+        /*public string toDec(char[] bin)    //method bin to dec
+        {
+            string input = new string(bin);
+            int decValue = Convert.ToInt32(input, 2);
+            string output = decValue.ToString();
+            return output;
+        }
+         */
+    }
+
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+
+        
+    }
+    protected void TextBox2_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+}
